9b21670cd99cb57f493c36261675db9dfaacb05f
bttttta <bt5akongqu@gmail.com>
2024-11-07 02:50:37 +0900

    fix Eventが格納されてなかったので格納するように

--- wotoge/Assets/Scripts/NotesManager.cs
+++ wotoge/Assets/Scripts/NotesManager.cs
@@ -96,6 +96,7 @@ public class NotesManager : SingletonMonoBehaviour<NotesManager> {
                     default:
                         break;
                 }
+                events.Add(nEvent);
             }
 
         }
